f8375139dc86ec4ed176112911a6f8a6f5e9c993
Moi10legend <moisesflopesf@gmail.com>
2024-12-22 21:05:35 -0300

    criação do primeiro projeto react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Button from "./components/button";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá React</h1>
+      <Button title="Clique aqui"></Button>
+    </div>
+  );
+}
+
+export default App;
